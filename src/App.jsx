853525d77e5285518dcb375d759b588759a3ab4b
unknown <munyaarmel61@gmail.com>
2023-02-23 08:39:46 +0200

    refactor ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './sass/main.scss'
 import Landing from './components/landing/Landing'
+
 // import Header from './components/header/header'
 function App() {
   return (
 <div className='app-container'>
-  {/* <Header/> */}
 <Landing />
     </div>
   )
